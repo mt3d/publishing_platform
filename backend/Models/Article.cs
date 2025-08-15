8670b1267ed7e09062c3fd2830e9fc2094f0ea38
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-08-15 16:08:43 +0300

    Add basic Article and User models

---
+++ backend/Models/Article.cs
@@ -0,0 +1,17 @@
+﻿namespace backend.Models
+{
+	public class Article
+	{
+		public int ArticleId { get; set; }
+		public string? Slug { get; set; }
+		public string? Title { get; set; }
+		public string? Subtitle { get; set; }
+		public string? Body { get; set; }
+		public User? Author { get; set; }
+
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
+
+		// TODO: Add tags, comments, favorites count, and the people who added this article as a favorite.
+	}
+}
---
+++ backend/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Models
+{
+	public class User
+	{
+		public int UserId { get; set; }
+		public string? Username { get; set; }
+		public string? Password { get; set; }
+		public string? Email { get; set; }
+		public string? Bio { get; set; }
+		public string? ProfilePic { get; set; }
+
+		// TODO: Add followers, following, and favorite articles
+	}
+}
