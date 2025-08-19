6ba7f225beddf727fbc9583b46ede621bee917f7
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-08-19 22:51:11 +0300

    Add data context to services and configure the sql server

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,17 @@
+using Azure.Identity;
+using backend.Logic;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // TODO: Add DbContext
+
+// TODO: Configure connection string and database provider
+builder.Services.AddDbContext<PlatformContext>(options =>
+{
+	// TODO: Add the ability to use other providers
+	options.UseSqlServer(builder.Configuration["ConnectionStrings:PlatformConnection"]);
+});
 
 // TODO: Add Localization
 
@@ -27,5 +38,10 @@ var app = builder.Build();
 app.MapControllers();
 
 // TODO: Use Swagger
+
+using (var scope = app.Services.CreateScope())
+{
+	var context = scope.ServiceProvider.GetRequiredService<PlatformContext>().Database.EnsureCreated();
+}
 
 app.Run();
